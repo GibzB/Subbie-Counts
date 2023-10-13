0a020ea9d1708e4b7870e1376093775c568a32ff
Billy Gibendi <ralphgibendi01@gmail.com>
2023-10-13 20:43:15 +0400

    🌈 style(channels): youtube channel formatting

--- ChatGPTsubbie/script.js
+++ ChatGPTsubbie/script.js
@@ -1,50 +1,35 @@
 // Sample channel data, you can add more channels
 const channels = [
-    // Andrew Kibe Subscribers
+    // Channel_TukoKenya Subscribers
     { url: "https://mixerno.space/embed/youtube-channel-counter/UCl6ICaB9meBcSMQ5Zmr446g?counter=0" },
-    // Andrew Kibe Subscribers
-    { name: "Channel 1", url: "https://mixerno.space/embed/youtube-channel-counter/UCiXj0BfxM0WQGFr9mxdR0qg" },
-    // Andrew Kibe Subscribers
-    { name: "Channel 1", url: "https://mixerno.space/embed/youtube-channel-counter/UCiXj0BfxM0WQGFr9mxdR0qg" },
-    // Andrew Kibe Subscribers
-    { name: "Channel 1", url: "https://mixerno.space/embed/youtube-channel-counter/UCMD-JmezXoQiV9PiWLwVpGQ" },
-    // Andrew Kibe Subscribers
-    { name: "Channel 1", url: "https://mixerno.space/embed/youtube-channel-counter/UCBZPPYU_l99Mg6-GXm6xbMg" },
-    // Andrew Kibe Subscribers
-    { name: "Channel 1", url: "https://mixerno.space/embed/youtube-channel-counter/UCiXj0BfxM0WQGFr9mxdR0qg" },
-    // Andrew Kibe Subscribers
-    { name: "Channel 1", url: "https://mixerno.space/embed/youtube-channel-counter/UCiXj0BfxM0WQGFr9mxdR0qg" },
-    // Andrew Kibe Subscribers
-    { name: "Channel 1", url: "https://mixerno.space/embed/youtube-channel-counter/UCiXj0BfxM0WQGFr9mxdR0qg" },
-    // Andrew Kibe Subscribers
-    { name: "Channel 1", url: "https://mixerno.space/embed/youtube-channel-counter/UCiXj0BfxM0WQGFr9mxdR0qg" },
-    // Andrew Kibe Subscribers
-    { name: "Channel 1", url: "https://mixerno.space/embed/youtube-channel-counter/UCiXj0BfxM0WQGFr9mxdR0qg" },
-    // Andrew Kibe Subscribers
-    { name: "Channel 1", url: "https://mixerno.space/embed/youtube-channel-counter/UCiXj0BfxM0WQGFr9mxdR0qg" },
-    // Andrew Kibe Subscribers
-    { name: "Channel 1", url: "https://mixerno.space/embed/youtube-channel-counter/UCiXj0BfxM0WQGFr9mxdR0qg" },
+    // Channel_EveMungai Subscribers
+    { url: "https://mixerno.space/embed/youtube-channel-counter/UCcBNWxvAXH-nTAzOeuuLHgw" },
+    // Channel_PresenterAli Subscribers
+    { url: "https://mixerno.space/embed/youtube-channel-counter/UCMD-JmezXoQiV9PiWLwVpGQ" },
+    // Channel_CrazyKennar Subscribers
+    { url: "https://mixerno.space/embed/youtube-channel-counter/UCiXj0BfxM0WQGFr9mxdR0qg" },
+    // Channel_SautiSol Subscribers
+    { url: "https://mixerno.space/embed/youtube-channel-counter/UCBZPPYU_l99Mg6-GXm6xbMg" },
+    // Channel_Bahati Subscribers
+    { url: "https://mixerno.space/embed/youtube-channel-counter/UC2O6vD7gjgZIXKVegD6qOHA" },
+    // Channel_Wajesus Subscribers
+    { url: "https://mixerno.space/embed/youtube-channel-counter/UCHVTpHXp43Giiwag5s6o8xQ" },
+    // Channel_DianaB Subscribers
+    { url: "https://mixerno.space/embed/youtube-channel-counter/UC0UslOx9EYxnk_4SFSHj8hA" },
+    // Channel_WillyPaul Subscribers
+    { url: "https://mixerno.space/embed/youtube-channel-counter/UCgdVgtJQXxebSiSAzlhYczw" },
+    // Channel_KhaligraphJones Subscribers
+    { url: "https://mixerno.space/embed/youtube-channel-counter/UCRRSCAz5VJQqJHAIPgFVCxw" },
+    // Channel_Desagu Subscribers
+    { url: "https://mixerno.space/embed/youtube-channel-counter/UC7MGnWilDcg157op0eKI1Tg?counter=0" },
+    // Channel_Njugush Subscribers
+    { url: "https://mixerno.space/embed/youtube-channel-counter/UCkV3t4uxBsi8jYj3wjqooeQ?counter=0" },
+    // Channel_AbelMutua Subscribers
+    { url: "https://mixerno.space/embed/youtube-channel-counter/UC4tjY2tTltEKePusozUxtSA" },
+    // Channel_LynnNgugi Subscribers
+    { url: "https://mixerno.space/embed/youtube-channel-counter/UCa2gldA2ivhzMwIJRA5683w?counter=0" },
     
-    // Andrew Kibe Subscribers
-    { name: "Channel 1", url: "https://mixerno.space/embed/youtube-channel-counter/UCiXj0BfxM0WQGFr9mxdR0qg" },
     
-    // Andrew Kibe Subscribers
-    { name: "Channel 1", url: "https://mixerno.space/embed/youtube-channel-counter/UCiXj0BfxM0WQGFr9mxdR0qg" },
-    // Andrew Kibe Subscribers
-    { name: "Channel 1", url: "https://mixerno.space/embed/youtube-channel-counter/UCiXj0BfxM0WQGFr9mxdR0qg" },
-    // Andrew Kibe Subscribers
-    { name: "Channel 1", url: "https://mixerno.space/embed/youtube-channel-counter/UCiXj0BfxM0WQGFr9mxdR0qg" },
-    // Andrew Kibe Subscribers
-    { name: "Channel 1", url: "https://mixerno.space/embed/youtube-channel-counter/UCiXj0BfxM0WQGFr9mxdR0qg" },
-    // Andrew Kibe Subscribers
-    { name: "Channel 1", url: "https://mixerno.space/embed/youtube-channel-counter/UCiXj0BfxM0WQGFr9mxdR0qg" },
-    // Andrew Kibe Subscribers
-    { name: "Channel 1", url: "https://mixerno.space/embed/youtube-channel-counter/UCiXj0BfxM0WQGFr9mxdR0qg" },
-    // Andrew Kibe Subscribers
-    { name: "Channel 1", url: "https://mixerno.space/embed/youtube-channel-counter/UCiXj0BfxM0WQGFr9mxdR0qg" },
-    // Andrew Kibe Subscribers
-    
-
     // Add more channels here
 ];
 
